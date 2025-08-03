a084c3598673577a967fe519fc713e4ada1737a2
Andrei Karanko <a.karanko@polytech.software>
2025-08-03 21:04:28 +0300

    Add UI libraries, theme, and sidebar layout scaffolding
    Integrates Clerk authentication and several UI libraries for enhanced component development, updates theming with an extended color palette using oklch, and introduces a sidebar layout structure to support future navigation and app organization. Improves ESLint/Prettier formatting and updates .gitignore for Clerk configuration.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,36 @@
+import {
+  SidebarProvider,
+  Sidebar,
+  SidebarHeader,
+  SidebarTrigger,
+  SidebarContent,
+  SidebarFooter,
+  SidebarMenu,
+  SidebarMenuItem,
+  SidebarMenuButton,
+} from '@/components/ui/sidebar';
+import { AppSidebarClient } from './_AppSidebarClient';
+
 export default function HomePage() {
-  return <h1>HI</h1>
+  return (
+    <SidebarProvider className='overflow-y-hidden'>
+      <AppSidebarClient>
+        <Sidebar collapsible='icon' className='overflow-hidden'>
+          <SidebarHeader className='flex-row'>
+            <SidebarTrigger></SidebarTrigger>
+            <span className='text-xl text-nowrap'>WDS Jobs</span>
+          </SidebarHeader>
+          <SidebarContent></SidebarContent>
+          <SidebarFooter>
+            <SidebarMenu>
+              <SidebarMenuItem>
+                <SidebarMenuButton>dsds</SidebarMenuButton>
+              </SidebarMenuItem>
+            </SidebarMenu>
+          </SidebarFooter>
+        </Sidebar>
+        <main className='flex-1'>dsdsdss</main>
+      </AppSidebarClient>
+    </SidebarProvider>
+  );
 }
